c2bd5f9f4efe0ff5319bf111335042c2ee5cdaf2
Vlad Proshchavaiev <f3joule@gmail.com>
2019-12-26 17:30:48 +0200

    Remove toolchain override from init-wasm

--- init-wasm.sh
+++ init-wasm.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-echo "*** Initialising WASM build environment"
+echo "*** Initializing WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ] ; then
    rustup update nightly
    rustup update stable
 fi
 
-rustup override set nightly-2019-07-14
-rustup target add wasm32-unknown-unknown --toolchain nightly-2019-07-14
+rustup target add wasm32-unknown-unknown --toolchain nightly
 
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
